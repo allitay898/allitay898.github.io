aa4dc2411fc3dc98f51d2387b1a8a6f75beb2c66
allitay898 <tay18014@byui.edu>
2020-10-24 16:51:44 -0600

    update

--- Lesson 6/js/windchill.js
+++ Lesson 6/js/windchill.js
@@ -1,13 +1,13 @@
 function windChill(){
     let t = parseFloat(document.getElementById("t").innerText);
     let s = parseFloat(document.getElementById("s").innerText);
-    let ouptut = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(2) : output = "N/A";;
+    let ouptut ="f";
 
-    /*if(t >= 50 && s > 10){
+    if(t >= 50 && s > 10){
     f = (35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16));
-    }*/
+    }
 
-    (t <= 50 && s > 10) ? output = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(2) : output = "N/A";
+    /*(t >= 50 && s > 10) ? output = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(2) : output = "f";*/
 document.getElementById("windChill").innerHTML = output;
 
 }
