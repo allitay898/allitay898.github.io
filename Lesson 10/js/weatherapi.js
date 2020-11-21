a38606bffc70df535a84ae1656115bda7d7b5aad
allitay898 <tay18014@byui.edu>
2020-11-21 15:13:39 -0700

    update

--- Lesson 10/js/weatherapi.js
+++ Lesson 10/js/weatherapi.js
@@ -11,7 +11,7 @@ fetch(apiURL)
 
 });
 
-const forapi = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0b655db4a8dd3c42d0038307528bbbb3&units=imperial";
+const forapi = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0b655db4a8dd3c42d0038307528bbbb3&units=imperial';
 
 fetch(forapi)
 .then((response) => response.json())
