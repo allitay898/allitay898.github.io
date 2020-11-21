f6f03379edc82d4cc0da29627fe441b0158f7b42
allitay898 <tay18014@byui.edu>
2020-11-21 13:10:16 -0700

    update

--- Lesson 10/js/windchill.js
+++ Lesson 10/js/windchill.js
@@ -1,13 +1,15 @@
 function windChill(){
     let t = parseFloat(document.getElementById("currentTemp").innerText);
     let s = parseFloat(document.getElementById("windSpeed").innerText);
-    let output = "";
 
-    /*if(t >= 50 && s > 10){
-    f = (35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16));
-    }
-    else f = "N/A"*/
 
-    (t <= 50 && s < 10) ? output = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(0) : output = "N/A"; document.getElementById("windchill").innerHTML = output;
- 
+// Calculate the Wind Chill Factor
+var windChillFactor = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+
+// Round windChillFactor
+var digits = 1;
+var multiplier = Math.pow(10, digits);
+var windChillFactorRounded = Math.round(windChillFactor * multiplier ) / multiplier;
+
+document.getElementById("windChill").innerHTML = windChillFactorRounded + "°F";
 }
