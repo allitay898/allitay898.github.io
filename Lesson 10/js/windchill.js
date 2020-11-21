43723e9198324c259ced25a2202cd6750327d735
allitay898 <tay18014@byui.edu>
2020-11-21 12:46:31 -0700

    update

--- Lesson 10/js/windchill.js
+++ Lesson 10/js/windchill.js
@@ -1,14 +1,13 @@
 function windChill(){
-    let t = parseFloat(document.getElementById("t").innerText);
-    let s = parseFloat(document.getElementById("s").innerText);
-    let output = f;
+    let t = parseFloat(document.getElementById("currentTemp").innerText);
+    let s = parseFloat(document.getElementById("windSpeed").innerText);
+    let output = "";
 
-    if(t >= 50 && s > 10){
+    /*if(t >= 50 && s > 10){
     f = (35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16));
     }
-    else f = "N/A"
+    else f = "N/A"*/
 
-    /*(t <= 50 && s > 10) ? output = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(2) : output = "N/A";*/
-document.getElementById("windChill").innerHTML = output;
-
+    (t <= 50 && s < 10) ? output = ((35.74+(0.6215 * t)) - (35.75*Math.pow(s,0.16))+(0.4275*t)*(Math.pow(s,0.16))).toFixed(0) : output = "N/A"; document.getElementById("windchill").innerHTML = output;
+ 
 }
