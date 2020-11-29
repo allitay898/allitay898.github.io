92e64edfa874eb080051881ba1c0faacf247b1b2
allitay898 <tay18014@byui.edu>
2020-11-28 18:21:59 -0700

    update

--- Lesson 11/js/towns.js
+++ Lesson 11/js/towns.js
@@ -1,3 +1,5 @@
+//let myPath = window.location.pathname;
+
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL) 
@@ -5,11 +7,13 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jasonObject){
+        //console.table(jsonObject);
 
         const towns = jasonObject['towns'];
 
         for (let i = 0; i < towns.length; i++ ){
             if(towns[i].name == 'Preston' || towns[i].name == 'Soda Springs'|| towns[i].name == 'Fish Haven'){
+                //if (myPath.includes("index.html")){
                 let town_info = document.createElement('info_blocks');
                 let name = document.createElement('h2');
                 let photo = document.createElement('img');
@@ -17,14 +21,14 @@ fetch(requestURL)
                 let yearFounded = document.createElement('p');
                 let currentPopulation = document.createElement('p');
                 let averageRainfall = document.createElement('p');
-                //let events = document.createElement('p');
+                let events = document.createElement('p');
 
                 name.textContent = towns[i].name;
                 motto.textContent = towns[i].motto;
                 yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
                 currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
                 averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-                //events.textContent = 'Events: ' + towns[i].events;
+                events.textContent = 'Events: ' + towns[i].events;
                 photo.setAttribute('src','images/'+ towns[i].photo);
                 photo.setAttribute("height", "205");
                 photo.setAttribute('alt', name.textContent + '-' + towns[i].order);
@@ -34,27 +38,10 @@ fetch(requestURL)
                 town_info.appendChild(yearFounded);
                 town_info.appendChild(currentPopulation);
                 town_info.appendChild(averageRainfall);
-                //town_info.appendChild(events);
+                town_info.appendChild(events);
                 town_info.appendChild(photo);
 
                 document.querySelector('div.town_info').appendChild(town_info);
             }
-            for (let t = 0; t < towns[i].events.length; t++) {
-                if (myPath.includes("preston.html") && towns[i].name == 'Preston') {
-                  let event = document.createElement('p');
-                  event.textContent = towns[i].events[t];
-                  document.querySelector('div.events').appendChild(event);
-                }
-                if (myPath.includes("fishhaven.html") && towns[i].name == 'Fish Haven') {
-                  let event = document.createElement('p');
-                  event.textContent = towns[i].events[t];
-                  document.querySelector('div.events').appendChild(event);
-                }
-                if (myPath.includes("sodasprings.html") && towns[i].name == 'Soda Springs') {
-                  let event = document.createElement('p');
-                  event.textContent = towns[i].events[t];
-                  document.querySelector('div.events').appendChild(event);
-                }
-              } 
         }
     })
