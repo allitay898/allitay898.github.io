dba27956d2de29ad890d26326b75dd56824b577e
allitay898 <tay18014@byui.edu>
2020-12-12 18:09:53 -0700

    update

--- Lesson 10/js/currentdate.js
+++ Lesson 10/js/currentdate.js
@@ -1,13 +1,5 @@
 try {
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
     document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
   }
   
-function showBanner(){
-    let today = new Date();
-    if(today.getDay() == 5){
-        document.getElementById("pancakes").style.display ="block";
-    }
-}
