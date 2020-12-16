2bd7b3c8ca4e665c492ff9effa6bd23c1a7c155f
allitay898 <tay18014@byui.edu>
2020-12-15 17:28:10 -0700

    UPDATE

--- Lesson 10/js/currentdate.js
+++ Lesson 10/js/currentdate.js
@@ -2,4 +2,3 @@ try {
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
     document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
   }
-  
