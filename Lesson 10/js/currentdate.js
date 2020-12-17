11113416f4d9dd4f69edb068be1794d1989d3a61
allitay898 <tay18014@byui.edu>
2020-12-16 18:25:59 -0700

    update

--- Lesson 10/js/currentdate.js
+++ Lesson 10/js/currentdate.js
@@ -1,4 +1,43 @@
-try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-  }
+function getWeekday() {
+  var dayNameList = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];      
+  return dayNameList[new Date().getDay()];
+}
+
+function getFullDateProper() {
+  var currentDate = new Date();
+  var dayNameList = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+    var monthNameList = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];  
+  return `${dayNameList[currentDate.getDay()]}, ${currentDate.getDate()} ${monthNameList[currentDate.getMonth()]} ${currentDate.getFullYear()}`; 
+}
+
+document.getElementsByClassName('copyright-year')[0].innerHTML = new Date().getFullYear();
+document.getElementsByClassName('current-date')[0].innerHTML = getFullDateProper();
